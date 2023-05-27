a73af0480b6ce3a5a161365cf0056fa4ba26c8b0
Michal <mbaka0002@gmail.com>
2023-05-27 14:23:32 +0200

    Setup connection to MongoDB

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = process.env.MONGO_URL || '';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@klastus.8deewuw.mongodb.net/taw_mini_projekt`
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
 
